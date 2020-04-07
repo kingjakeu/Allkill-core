ec9638a8f4952d8c286b31d729a99b5707dc6736
kingjake <jakeyoo12@gmail.com>
2020-04-07 21:33:26 +0900

    change timeouts

--- src/main/java/com/kingjakeu/allkillcore/domain/coupang/CoupangCapacityService.java
+++ src/main/java/com/kingjakeu/allkillcore/domain/coupang/CoupangCapacityService.java
@@ -48,7 +48,7 @@ public class CoupangCapacityService {
     }
 
 
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRate = 10000)
     public void crawlCoupang() {
         Connection connection = this.getConnection();
         try{
@@ -69,7 +69,7 @@ public class CoupangCapacityService {
 
     public Connection getConnection(){
         return Jsoup.connect("https://www.coupang.com/vp/products/1384804427")
-                .timeout(3000)
+                .timeout(30000)
                 .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'")
                 .header("accept-encoding", "gzip, deflate, br")
                 .header("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
